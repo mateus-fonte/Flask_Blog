4e28c06ecc82df2a5e49911a08255da20cfee912
Mateus da Fonte <mateusdafonte@outlook.com>
2025-09-15 22:59:52 -0300

    Adding templates for the home and about pages, also adding dummy posts to the home page

--- src/flaskblog.py
+++ src/flaskblog.py
@@ -1,15 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author'     : 'Mateus da Fonte',
+        'title'      : 'Blog Post 1',
+        'content'    : 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+        {
+        'author'     : 'Julinho Babalu',
+        'title'      : 'Blog Post 2',
+        'content'    : 'Second post content',
+        'date_posted': 'May 20, 2018'
+    },
+        {
+        'author'     : 'Juan Carlos',
+        'title'      : 'Blog Post 3',
+        'content'    : 'Third post content',
+        'date_posted': 'January 20, 2018'
+    }
+]
 
 @app.route("/")                         # route anotation/decorator
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")                         # route anotation/decorator
 def about():
-    return "<h1>About Page</h1>"
-
+    return render_template("about.html",title='About')
 if __name__ == "__main__":
     app.run(debug=True)
